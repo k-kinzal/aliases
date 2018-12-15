5d79539a654fe262c79e0b03e84722d57b11ae74
cw-ozaki <log@logn.in>
2018-12-15 13:36:50 +0900

    add commit hash into dev version suffix

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,12 +1,19 @@
 package version
 
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
 var (
 	version string
 )
 
 func GetVersion() string {
 	if version == "" {
-		return "dev"
+		out, _ := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
+		return fmt.Sprintf("dev-%s", strings.Trim(string(out), "\r\n"))
 	} else {
 		return version
 	}
