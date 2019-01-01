078766043d8511f31891fdbd702f1d8c3441900e
cw-ozaki <log@logn.in>
2019-01-01 21:40:17 +0900

    add docker downloader

--- test/integration/docker-binary/test.sh
+++ test/integration/docker-binary/test.sh
@@ -9,13 +9,9 @@ ALIASES=$(cd "${TEST_DIR}/../../..//dist"; echo "$(pwd)/aliases -c ${TEST_DIR}/a
 DIFF=$(if which colordiff >/dev/null; then echo "colordiff -Buw --strip-trailing-cr"; else echo "diff -Bw"; fi)
 MASK="sed -e s|${HOME}|[HOME]|g -e s|${TEMP_DIR}|[TEMP_DIR]|g"
 
-USER=root
-DOCKER_MOUNT_TYPE=volume
-DOCKER_MOUNT_DIR=$PWD
-
 ${ALIASES} gen --export-path "${TEMP_DIR}" | ${MASK} | sort | ${DIFF} ${TEST_DIR}/alias -
 ${ALIASES} gen --export --export-path "${TEMP_DIR}" | ${MASK} | ${DIFF} ${TEST_DIR}/export -
-cat ${TEMP_DIR}/alpine | ${MASK} | ${DIFF} ${TEST_DIR}/alpine -
+cat ${TEMP_DIR}/ubuntu | ${MASK} | ${DIFF} ${TEST_DIR}/ubuntu -
 
-${TEMP_DIR}/alpine /bin/sh -c "echo foo" | ${DIFF} ${TEST_DIR}/stdout -
-${ALIASES} run /usr/local/bin/alpine /bin/sh -c "echo foo" | ${MASK} | ${DIFF} ${TEST_DIR}/stdout -
+${TEMP_DIR}/ubuntu /bin/sh -c "docker -v" | ${DIFF} ${TEST_DIR}/stdout -
+${ALIASES} run /usr/local/bin/ubuntu /bin/sh -c "docker -v" | ${DIFF} ${TEST_DIR}/stdout -
