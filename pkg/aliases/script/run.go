cac1c6c167905a88b65f32077c8762217bd69bf4
k-kinzal <log@logn.in>
2019-02-17 03:06:30 +0900

    add timeout in aliases run

--- pkg/aliases/script/run.go
+++ pkg/aliases/script/run.go
@@ -6,13 +6,14 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/k-kinzal/aliases/pkg/util"
 
 	"github.com/k-kinzal/aliases/pkg/posix"
 
-	"github.com/k-kinzal/aliases/pkg/logger"
-	"github.com/k-kinzal/aliases/pkg/util"
-
 	"github.com/k-kinzal/aliases/pkg/docker"
+	"github.com/k-kinzal/aliases/pkg/logger"
 )
 
 // Run aliases script.
@@ -26,7 +27,7 @@ func (script *Script) Run(args []string, opt docker.RunOption) error {
 	dockerCmdString := script.docker(args, opt).String()
 	logger.Debug(dockerCmdString)
 
-	command := posix.Shell(fmt.Sprintf("echo \"\" >/dev/null | %s", dockerCmdString))
+	command := posix.Shell(dockerCmdString)
 	command.Env = os.Environ()
 
 	info, err := os.Stdin.Stat()
@@ -46,32 +47,49 @@ func (script *Script) Run(args []string, opt docker.RunOption) error {
 		return nil
 	}
 
-	if err := command.Start(); err != nil {
-		return err
-	}
+	fn := func() error {
+		if err := command.Start(); err != nil {
+			return err
+		}
 
-	scanner := bufio.NewScanner(stderr)
-	for scanner.Scan() {
-		line := scanner.Text()
-		// invalid argument error converts go error
-		if strings.HasPrefix(line, "invalid argument") {
-			r := regexp.MustCompile(`^invalid argument "(.*?)" for "(.*?)" flag: (.*)$`)
-			matches := r.FindStringSubmatch(line)
-			if len(matches) > 0 {
-				return util.FlagError(matches[1], matches[2], matches[3])
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			line := scanner.Text()
+			// invalid argument error converts go error
+			if strings.HasPrefix(line, "invalid argument") {
+				r := regexp.MustCompile(`^invalid argument "(.*?)" for "(.*?)" flag: (.*)$`)
+				matches := r.FindStringSubmatch(line)
+				if len(matches) > 0 {
+					return util.FlagError(matches[1], matches[2], matches[3])
+				}
 			}
+			// remove docker prefix
+			if strings.HasPrefix(line, "docker: Error response from daemon: ") {
+				fmt.Fprintln(os.Stderr, "aliases: error response from daemon: "+strings.TrimPrefix(line, "docker: Error response from daemon: "))
+				continue
+			}
+			// suppress guidance to help
+			if line == "See 'docker run --help'." {
+				continue
+			}
+			fmt.Fprintln(os.Stderr, line)
 		}
-		// remove docker prefix
-		if strings.HasPrefix(line, "docker: Error response from daemon: ") {
-			fmt.Fprintln(os.Stderr, "aliases: error response from daemon: "+strings.TrimPrefix(line, "docker: Error response from daemon: "))
-			continue
-		}
-		// suppress guidance to help
-		if line == "See 'docker run --help'." {
-			continue
-		}
-		fmt.Fprintln(os.Stderr, line)
+
+		return command.Wait()
 	}
 
-	return command.Wait()
+	if (info.Mode()&os.ModeNamedPipe) != 0 && info.Size() == 0 {
+		// FIXME: timeout get from command line arguments
+		if err := util.Timeout(3*time.Second, fn); err != nil {
+			if _, ok := err.(*util.TimeoutError); ok {
+				_ = command.Process.Kill()
+				return nil
+			}
+			return err
+		}
+	} else {
+		return fn()
+	}
+
+	return nil
 }
